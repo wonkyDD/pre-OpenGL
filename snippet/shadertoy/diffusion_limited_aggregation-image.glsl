0c95d861dc46ab7d1fc32db1127b46e998a985c2
wonkydd <dev.csh.01@gmail.com>
2022-11-07 19:31:50 +0900

    :fire: restart shadertoy

--- snippet/shadertoy/diffusion_limited_aggregation-image.glsl
+++
@@ -1,9 +0,0 @@
-// https://www.shadertoy.com/view/ldK3RW
-
-// 2016 David A Roberts <https://davidar.io>
-
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-	vec2 uv = fragCoord.xy / iResolution.xy;
-	fragColor = (texture(iChannel0,uv) + texture(iChannel1,uv) + texture(iChannel2,uv) + texture(iChannel3,uv)) / 5.;
-}
--- snippet/shadertoy/mystify_screensaver-image.glsl
+++
@@ -1,9 +0,0 @@
-// https://www.shadertoy.com/view/MsKcRh
-
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-    // Normalized pixel coordinates (from 0 to 1)
-    vec2 uv = fragCoord/iResolution.xy;
-
-    fragColor = texture(iChannel0, uv);
-}
