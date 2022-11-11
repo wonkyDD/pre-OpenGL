a0da4548c878c5a493f3d4999bfb8f83353d4b56
wonkydd <dev.csh.01@gmail.com>
2022-11-11 22:20:25 +0900

    :construction: WIP: multiple program, shader, vertex array/buffer issue

---
+++ snippet/alkagi/1.2.1-hello_triangle_finish.cpp
@@ -0,0 +1,14 @@
+// https://learnopengl.com/Getting-started/Hello-Triangle
+
+/**
+ * @todo
+ * 1. shader program 2개 사용
+ * 
+ * 2. program은 각각 다른 fragment shader 사용
+ * (기존 main.fs와 맨처음 fs를 main2.fs에 넣을 것)
+ * 
+ * 3. vao, vbo 2개씩 생성
+ * 
+ * 4. 각 vao, vbo 쌍으로 첫번째는 평범한 직각삼각형 (vertex attribute로 color를 넣어주는),
+ * 두번째는 맨처음 greenValue를 uniform으로 넣어주는 직각삼각형 처리
+*/
