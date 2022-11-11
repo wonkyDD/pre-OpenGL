61ea1ba91d50dab53ecfe14d6a385e905766b7fd
wonkydd <dev.csh.01@gmail.com>
2022-11-11 16:03:51 +0900

    :bulb: add comment

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 # 1. vscode 에서 tasks.json에 바로 build.sh 실행시키기?
 # 2. main 바이너리는 build 폴더에 생성
 # 3. 빌드이후 cd build && ./main 하는 식으로 설정
+
+
+# 추후에 snippet 또한 빌드될수있게 cmake수정해줘야함
+# 아마 LearnOpenGL처럼 폴더안에 shader, sorucefile을 같이 넣는 구조로 해야할듯.
+# (현재 구조에서 대규모 수정필요 snippet/alkagi/)
