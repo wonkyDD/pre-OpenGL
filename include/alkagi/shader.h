2d5679ac138d31ef1c9d2cd70fed4e5b06954bb9
wonkydd <dev.csh.01@gmail.com>
2022-11-12 09:07:34 +0900

    :art: add newlines/spaces

--- include/alkagi/shader.h
+++ include/alkagi/shader.h
@@ -12,6 +12,7 @@ class Shader
 {
 public:
     unsigned int ID;
+
     Shader(const char* vertexPath, const char* fragmentPath)
     {
         std::string vertexCode;
@@ -66,22 +67,27 @@ public:
         glDeleteShader(vertex);
         glDeleteShader(fragment);
     }
+
     void use() 
     { 
         glUseProgram(ID); 
     }
+
     void setBool(const std::string &name, bool value) const
     {         
         glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
     }
+
     void setInt(const std::string &name, int value) const
     { 
         glUniform1i(glGetUniformLocation(ID, name.c_str()), value); 
     }
+
     void setFloat(const std::string &name, float value) const
     { 
         glUniform1f(glGetUniformLocation(ID, name.c_str()), value); 
     }
+    
 private:
     void checkCompileErrors(unsigned int shader, std::string type)
     {
