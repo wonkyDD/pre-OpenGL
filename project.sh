48f2786a2ba4e0b715653ad15f0333964573a7f8
wonkydd <dev.csh.01@gmail.com>
2022-11-22 03:13:01 +0900

    :pencil2: fix

--- project.sh
+++ project.sh
@@ -1,6 +1,6 @@
 # https://learnopengl.com/In-Practice/2D-Game/Breakout
 # 원래는 2dgame만 생각했으나
-# 중간중간 tetris-2d, gomoku-2d, alkagi-2d 정도는 가능할것으로 보임
+# 중간중간 gomoku-2d, alkagi-2d 정도는 가능할것으로 보임
 # 1. rm -rf snippet/[프로젝트이름]
 # 2. mkdir -p snippet/[프로젝트이름]
 # 3. cp -R src/ snippet/[프로젝트이름]
